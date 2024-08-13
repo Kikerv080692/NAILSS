c1f931fc2cf20dcea639bb0e189bcb79ce8a17d0
Kikerv080692 <dp080692pva@gmail.com>
2024-08-13 17:01:52 +0200

    fix nails rendaring

--- src/components/Nails/Nails.jsx
+++ src/components/Nails/Nails.jsx
@@ -1,25 +1,33 @@
-import nails from "../data/nails.js";
-import Flickity from "react-flickity-component";
-import "./Nails.scss";
-import * as SC from "./Nails.styled.js";
-import {  useTranslation } from "react-i18next";
+import nails from '../data/nails.js';
+import Flickity from 'react-flickity-component';
+import './Nails.scss';
+import * as SC from './Nails.styled.js';
+import { useTranslation } from 'react-i18next';
+import { useEffect, useRef } from 'react';
 
 const flickityOptions = {
   initialIndex: 2,
 };
 
 function Nails() {
-  const{t} = useTranslation()
+  const { t } = useTranslation();
+  const flickityRef = useRef(null);
+
+  useEffect(() => {
+    if (flickityRef.current) {
+      new Flickity(flickityRef.current, flickityOptions);
+    }
+  }, []);
   return (
     <SC.Works className="container-works">
       <SC.Title className="container-works-title">
         <h1>{t('Works')}</h1>
       </SC.Title>
-      <div>
+      <div ref={flickityRef}>
         <Flickity
           className="Slider"
           elementType="div"
-          disableImagesLoaded={false}
+          disableImagesLoaded={true}
           options={flickityOptions}
           reloadOnUpdate
           static
@@ -27,16 +35,14 @@ function Nails() {
           {nails.map((nail, id) => {
             return (
               <div key={id} className="Plate">
-                <>
-                  <div
-                    style={{
-                      backgroundImage: `url(${nail.img})`,
-                      width: "100%",
-                      height: 600,
-                      backgroundSize: "cover",
-                    }}
-                  ></div>
-                </>
+                <div
+                  style={{
+                    backgroundImage: `url(${nail.img})`,
+                    width: '100%',
+                    height: 400,
+                    backgroundSize: 'cover',
+                  }}
+                ></div>
               </div>
             );
           })}
