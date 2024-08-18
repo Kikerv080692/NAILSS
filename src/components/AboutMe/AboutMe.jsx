128fe4fbdd3919cc1a6e3a4dc4a5ae0a6de89c52
Kikerv080692 <dp080692pva@g,ail.com>
2024-08-18 05:16:39 +0300

    add fix me

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,16 +1,17 @@
 import * as SC from './AboutMe.styled'
-
+import { useTranslation } from 'react-i18next';
 export const AboutMe = () => {
+  const { t } = useTranslation();
   return (
     <SC.Wrapper>
       <SC.WrapperText>
-        <h2>Чому саме я</h2>
+        <h2>{t('aboutMe')}</h2>
       </SC.WrapperText>
       <SC.WrapperText>
-        <p>Я дотримуюсь усіх санітарних норм.</p>
+        <p>{t('aboutMe1')}</p>
       </SC.WrapperText>
       <SC.WrapperText>
-        <p>Більш ніж 100 лаків та різних дизайнів.</p>
+        <p>{t('aboutMe2')}</p>
       </SC.WrapperText>
       <SC.WrapperText>
         <p>Манікюр за 2 години, педикюр за 1.5 години.</p>
